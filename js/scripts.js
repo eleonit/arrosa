b35e1b4716d0a6f4349cdfe8d1a38801b28a8dc1
eleonit <81837768+eleonit@users.noreply.github.com>
2021-05-12 19:24:03 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+
+let navbar = document.querySelector('#navbar');
+
+let menu_bar = document.querySelector('#menu-bar');
+
+    menu_bar.addEventListener('click', function() {
+
+        navbar.classList.toggle('navbar-toggle');
+    })
